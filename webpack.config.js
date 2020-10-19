748161b49235a6e7bbc595cdb6d23e8c2d2728f1
Mikhail Brazhnik <mikbrazh@gmail.com>
2020-10-19 15:35:19 +0300

    fix CopyPlugin
    See https://github.com/webpack-contrib/copy-webpack-plugin/tree/v5.1.1

--- webpack.config.js
+++ webpack.config.js
@@ -88,14 +88,13 @@ const plugins = () => {
       }
     }),
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
+    new CopyWebpackPlugin([
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
           to: path.resolve(__dirname, 'dist')
         }
       ]
-    }),
+    ),
     new MiniCssExtractPlugin({
       filename: filename('css')
     })
